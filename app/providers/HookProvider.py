d36db77296779ae3c812b49cb75572bb932f49dc
Tony Hammack <hammack.tony@gmail.com>
2018-10-02 17:50:39 -0500

    Added Raven package for sentry support.

--- app/providers/HookProvider.py
+++ app/providers/HookProvider.py
@@ -1,6 +1,6 @@
 ''' A HookProvider Service Provider '''
 from masonite.provider import ServiceProvider
-from ..hooks.Sentry import SentryHook
+from app.hooks.Sentry import SentryHook
 
 class SentryServiceProvider(ServiceProvider):
     def register(self):
