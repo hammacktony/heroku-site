da6c57e72b0730f01e13b59a566815ddf978fe49
Tony Hammack <hammack.tony@gmail.com>
2018-10-24 20:57:14 -0500

    Updated index page. Add a profile spotlight.

--- app/http/controllers/HomeController.py
+++ app/http/controllers/HomeController.py
@@ -1,9 +1,14 @@
 ''' A Module Description '''
+from app.User import User
 from masonite.facades.Auth import Auth
 
 class HomeController:
     ''' Class Docstring Description '''
 
-    def show(self, Request, Application):
+    def show(self):
         ''' Show Index Template '''
-        return view('index', {'app': Application})
+        
+        # Get my identity
+        user = User.where('id', '1').get()
+        
+        return view('index', {'user': user[0]})
