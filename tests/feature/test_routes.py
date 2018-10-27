a62b1d197499d934ab1824f33f2d4fe31f860f3a
Tony Hammack <hammack.tony@gmail.com>
2018-10-27 11:58:05 -0500

    Deselected running wsgi tests for travis.

--- tests/feature/test_routes.py
+++ tests/feature/test_routes.py
@@ -1,7 +1,10 @@
+import pytest
+
 from wsgi import application
 from webtest import TestApp as _TestApp
 
 
+@pytest.mark.webtest
 class TestRoutes(object):
 
     def setup_method(self):
@@ -15,11 +18,11 @@ class TestRoutes(object):
     def test_contact(self):
         resp = self.app.get('/contact')
         assert resp.status_int == 200
-        
+
     def test_projects(self):
         resp = self.app.get('/projects')
         assert resp.status_int == 200
-        
+
     def test_volcanoes(self):
         resp = self.app.get('/volcanoes')
         assert resp.status_int == 200
@@ -44,5 +47,3 @@ class TestRoutes(object):
     def test_blog_author(self):
         resp = self.app.get('/blog/author/tonus')
         assert resp.status_int == 200
-
-     
