41408ac1aa1a0d0ed3ada25de06bbb4a4692b785
Tony Hammack <hammack.tony@gmail.com>
2018-12-27 19:54:26 -0600

    Dynamic Blog Templates

--- app/http/controllers/PostsController.py
+++ app/http/controllers/PostsController.py
@@ -8,22 +8,22 @@ from masonite.facades.Auth import Auth
 class PostsController(object):
     ''' Posts Controller '''
 
-    def __init__(self, Request):
+    def __init__(self, Request, BlogRepo):
         """ Set blog table at runtime """
-        self.name = Request.param('blog')
+        self.route_name = Request.param('blog')
+        self.Blog = BlogRepo.get(self.route_name)
 
-    def show_all(self, BlogRepo):
+    def show_all(self):
         """ Controller to show all posts"""
-        Blog = BlogRepo.get(self.name)
-        # dd(Blog)
-        posts = Blog.where('is_live', 1).get()
-        return view('blog', {'author': User, 'posts': posts})
+
+        posts = self.Blog.where('is_live', 1).get()
+        return view('blog', {'author': User, 'posts': posts, 'route': self.route_name})
 
     def show_one(self, Request, RenderEngine):
         """ Controller to show single post"""
 
         # Get post via slug
-        posts = Post.where('slug', Request.param('slug')
+        posts = self.Blog.where('slug', Request.param('slug')
                            ).where('is_live', 1).get()
 
         # Render blog content
@@ -33,24 +33,24 @@ class PostsController(object):
         user = User.where('id', posts[0].author_id).get()
 
         # Get recent posts
-        recent_posts = Post.where('is_live', 1).order_by(
+        recent_posts = self.Blog.where('is_live', 1).order_by(
             'created_at', 'desc').take(5).get()
 
-        return view('blog/post', {'user': user[0], 'post': posts[0], 'recent': recent_posts})
+        return view('blog/post', {'user': user[0], 'post': posts[0], 'recent': recent_posts, 'route': self.route_name})
 
     def show_category(self, Request):
         """ Controller to show posts by category"""
 
         category = convert_slug_to_category(Request.param('category'))
-        posts = Post.where('category', category).where('is_live', 1).get()
+        posts = self.Blog.where('category', category).where('is_live', 1).get()
 
-        return view('blog/category', {'author': User, 'category': category, 'posts': posts})
+        return view('blog/category', {'author': User, 'category': category, 'posts': posts, 'route': self.route_name})
 
     def show_author(self, Request):
         """ Controller to show posts by author"""
 
         author = User.where('user_name', Request.param('author')).get()
 
-        posts = Post.where('author_id', author[0].id).where('is_live', 1).get()
+        posts = self.Blog.where('author_id', author[0].id).where('is_live', 1).get()
 
-        return view('blog/author', {'author': author[0], 'posts': posts})
+        return view('blog/author', {'author': author[0], 'posts': posts, 'route': self.route_name})
