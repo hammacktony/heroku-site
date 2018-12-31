f342f9e0894a0deeb8e4d6524586bd254c7aa9a9
Tony Hammack <hammack.tony@gmail.com>
2018-12-31 11:01:58 -0600

    Cleaned up clutter

--- config/urlshortener.py
+++ config/urlshortener.py
@@ -14,8 +14,6 @@ DRIVER = os.getenv('URL_SHORTENER', 'bitly')
 
 URL_SHORTENER = {
     "bitly": {
-        "api_user": os.getenv("BITLY_API_USER", None),
-        "api_key": os.getenv("BITLY_API_PASSWORD", None),
         "token": os.getenv("BITLY_API_TOKEN", None)
     }
 }
