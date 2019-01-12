67f86d892da1714321ec7bb539a20ce1f53fbcf5
Tony Hammack <hammack.tony@gmail.com>
2019-01-12 10:37:41 -0600

    Render markdown for XML

--- app/http/controllers/FeedController.py
+++ app/http/controllers/FeedController.py
@@ -7,10 +7,16 @@ class FeedController:
         self.blog_name = Request.param('blog').lower()
         self.Blog = BlogRepo.get(self.blog_name)
 
-    def show(self, Request):
+    def show(self, Request, RenderEngine):
+        """ Show XML Feed """
 
         posts = self.Blog.order_by(
             'created_at', 'desc').get()
+
+        # Render Markdown
+        for post in posts:
+            post.body = RenderEngine(post.body)
+
         Request.header('Content-Type', 'application/xml', http_prefix=None)
 
-        return view('feed/blog.xml', {'posts': posts, "blog_name": self.blog_name, })
+        return view('feed/blog.xml', {'posts': posts, "blog_name": self.blog_name})
