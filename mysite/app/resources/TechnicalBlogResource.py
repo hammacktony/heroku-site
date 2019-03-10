8f8c13cb9d16fa3a108bb8d5f8665a7df58628d0
Tony Hammack <hammack.tony@gmail.com>
2019-03-09 18:45:14 -0600

    Can now create posts

--- mysite/app/resources/TechnicalBlogResource.py
+++ mysite/app/resources/TechnicalBlogResource.py
@@ -13,6 +13,9 @@ class TechnicalBlogResource(Resource, JSONSerializer):
     model = TechnicalBlog
     # list_middleware = ['csrf']
 
+    # Url of blog
+    url = "http://www.tonyhammack.com/blog/tech/post/{}"
+
     def index(self, request: Request):
         """ Return all posts """
 
@@ -48,7 +51,7 @@ class TechnicalBlogResource(Resource, JSONSerializer):
 
         return self.model.where(query).get()
 
-    def create(self, UrlShortener, request: Request):  # , upload: Upload): 
+    def create(self, UrlShortener, request: Request):  # , upload: Upload):
         """ Create new post """
 
         # Validate Entry
@@ -68,35 +71,56 @@ class TechnicalBlogResource(Resource, JSONSerializer):
         #         request.input('file_upload'), 'blog/img/')
 
         #     # image = Upload.driver('s3').store_prepend(
-        #     #     Request.input('file_upload'), '{}/img/'.format(self.blog_name))
+        #     #     Request.input('file_upload'), 'tech/img/')
         # except AttributeError:
         #     # If user did not pick image, set image to none.
         #     image = None
 
-        slug = slugify(remove_whitespaces(request.input('title')))
-
-        # Get full url of article
-        url = f"http://www.tonyhammack.com/blog/tech/post/{slug}"
+        title = remove_whitespaces(request.input('title'))
+        slug = slugify(title)
+        body = remove_whitespaces(request.input('body'))
+        category = remove_whitespaces(request.input('category'))
 
         # Create shortened link for sharing
-        # shortened_url = UrlShortener.shorten(long_url=url)
-        # try:
-        #     link = shortened_url["link"]
-        # except KeyError:
-        #     link = None
-            
-        # # Create blog
-        # self.model.create(
-        #     title=remove_whitespaces(request.input('title')),
-        #     slug=slug,
-        #     category=remove_whitespaces(request.input('category')),
-        #     body=remove_whitespaces(request.input('body')),
-        #     image=None,
-        #     author_id=1, # TODO - Dynamically set author,
-        #     shortened_url=link,
-        #     is_live=1
-        # )
+        short_link = UrlShortener.shorten(long_url=self.url.format(slug))
+
+        post = {
+            'title': title,
+            'slug': slug,
+            'body': body,
+            'category': category,
+            'image': None,    # TODO - Change in the future
+            'author_id': 1,   # TODO - Change in the future
+            'shortLink': short_link.get('link', None),
+            'is_live': 1,
+        }
+
+        # Create blog
+        self.model.insert(post)
 
         request.status(201)
-        # return {'response': 'post created'}
-        return "Hi"
+        return {'response': 'post created'}
+
+    def update(self, UrlShortener, request: Request):
+        " Update Post "
+
+        # # Get post via slug
+        post = self.model.where(
+            'slug', request.param('slug')).get().serialize()[0]
+
+        # Updates Post
+        update = {}
+        update['title'] = remove_whitespaces(request.input('title'))
+        update['slug'] = slugify(update['title'])
+        update['body'] = remove_whitespaces(request.input('body'))
+        update['category'] = remove_whitespaces(request.input('category'))
+
+        # # Get full url of article
+        url = "http://www.tonyhammack.com/blog/tech/post/{update.slug}"
+
+        # # Create shortened link for sharing
+        shortened_url = UrlShortener.shorten(long_url=url)
+        update['shortLink'] = shortened_url.get("link", None)
+
+        self.model.where('id', post['id']).update(update)
+        return {'status': 'saved'}
