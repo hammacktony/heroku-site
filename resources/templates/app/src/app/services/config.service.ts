0c185d303639e0806e3aeb5e8c8c98ed22a62489
Tony Hammack <hammack.tony@gmail.com>
2019-08-15 21:26:46 -0500

    rebuild

--- resources/templates/app/src/app/services/config.service.ts
+++ resources/templates/app/src/app/services/config.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ConfigService {
     public static get S3_LOCATION(): string { return "//tony-hammack.s3-website.us-east-2.amazonaws.com" }
-    public static get API_URL(): string { return `${document.location.protocol}//${document.location.hostname}:8000` }
+    public static get API_URL(): string { return `${document.location.protocol}//${document.location.hostname}:80` }
 
     constructor() { }
 }
