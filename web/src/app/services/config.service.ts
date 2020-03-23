49aacd67ad9e47baba92d0fc684dbc533107e2a8
Tony Hammack <hammack.tony@gmail.com>
2020-03-23 15:39:10 -0500

    New s3 bucket

--- web/src/app/services/config.service.ts
+++ web/src/app/services/config.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class ConfigService {
-    public static get S3_LOCATION(): string { return "//tony-hammack.s3-website.us-east-2.amazonaws.com" }
+    public static get S3_LOCATION(): string { return "//th-website.s3-website.us-east-2.amazonaws.com" }
     public static get API_URL(): string { return `${document.location.protocol}//${document.location.hostname}` }
 
     constructor() { }
