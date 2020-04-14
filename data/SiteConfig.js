1721b87a5ebd71d07e22c4c977aa220c93e5d3a3
Tony Hammack <hammack.tony@gmail.com>
2020-04-13 21:17:36 -0500

    Update

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -24,7 +24,7 @@ const config = {
   themeColor: '#c62828', // Used for setting manifest and progress theme colors.
   backgroundColor: 'red', // Used for setting manifest background color.
   backgroundImages: {
-    general: 'https://th-website.s3.us-east-2.amazonaws.com/user/img/cover3.jpg',
+    general: 'http://th-website.s3.us-east-2.amazonaws.com/user/img/cover3.jpg',
     personal: 'http://th-website.s3-website.us-east-2.amazonaws.com/blog/img/blog-header.jpg',
     tech: 'http://th-website.s3-website.us-east-2.amazonaws.com/blog/img/laptop-blog.jpg'
   }
